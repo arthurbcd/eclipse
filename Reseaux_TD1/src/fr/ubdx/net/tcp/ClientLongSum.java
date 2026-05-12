def5e8efd6ae12aa9454f90a5a09e14c6c631fb7
Arthur Miranda <arthur.bcd@hotmail.com>
2026-05-12 11:07:52 +0200

    up

---
+++ Reseaux_TD1/src/fr/ubdx/net/tcp/ClientLongSum.java
@@ -0,0 +1,62 @@
+package fr.ubdx.net.tcp;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.util.Scanner;
+
+public class ClientLongSum {
+    public static void usage() {
+        System.out.println("Usage : ClientLongSum host port");
+    }
+
+    public static void main(String[] args) throws IOException {
+        if (args.length != 2) {
+            usage();
+            return;
+        }
+
+        String host = args[0];
+        int port = Integer.parseInt(args[1]);
+
+        InetSocketAddress isa = new InetSocketAddress(host, port);
+
+        try (SocketChannel sc = SocketChannel.open(isa);
+             Scanner scanner = new Scanner(System.in)) {
+
+            System.out.println("nbOperands?");
+
+            while (scanner.hasNextInt()) {
+                int nbOperands = scanner.nextInt();
+
+                ByteBuffer bbRequest = ByteBuffer.allocate(Integer.BYTES + nbOperands * Long.BYTES);
+                bbRequest.putInt(nbOperands);
+
+                for (int i = 0; i < nbOperands; i++) {
+                    System.out.println("Le " + (i + 1) + "º operand:");
+                    bbRequest.putLong(scanner.nextLong());
+                }
+
+                bbRequest.flip();
+                sc.write(bbRequest);
+
+                ByteBuffer bbResponse = ByteBuffer.allocate(Long.BYTES);
+                while (bbResponse.hasRemaining()) {
+                    if (sc.read(bbResponse) == -1) {
+                        System.out.println("Connection with server lost.");
+                        return;
+                    }
+                }
+
+                // en chunks / merceau.
+
+                bbResponse.flip();
+                System.out.println("------------------------------");
+                System.out.println("Sum: " + bbResponse.getLong());
+                System.out.println("------------------------------");
+                System.out.println("\n nbOperands?");
+            }
+        }
+    }
+}
